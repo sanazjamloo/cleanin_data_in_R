83824af63ba7356e9f63541d8b4cc3c4f9be3208
sanazjamloo <sanaz.jamloo@gmail.com>
2020-11-04 14:46:08 -0800

    add date functions

---
+++ converting_dates.R
@@ -0,0 +1,21 @@
+install.packages('lubridate')
+
+library(tidyverse)
+library(lubridate)
+
+setwd("/Users/sanaz/Downloads/Ex_Files_Cleaning_Bad_Data_R/Exercise Files/3_1")
+
+weather <- read_csv("mexicanweather.csv")
+
+summary(weather)
+dim(weather)
+
+# Creating a new year, month, and day column in the weather tibble
+weather$year <- year(weather$date)
+weather$month <- month(weather$date)
+weather$day <- day(weather$date)
+
+weather
+
+wday("2018-04-01")
+yday("2018-04-01")
