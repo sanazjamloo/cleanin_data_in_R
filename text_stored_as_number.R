0f54141df272c7596124aab11daf3e2c08d76180
sanazjamloo <sanaz.jamloo@gmail.com>
2020-11-04 16:38:21 -0800

    add text stored as number

---
+++ text_stored_as_number.R
@@ -0,0 +1,17 @@
+# text stored as numbers
+
+
+library(tidyverse)
+library(lubridate)
+
+setwd("/Users/sanaz/Downloads/Ex_Files_Cleaning_Bad_Data_R/Exercise Files/3_4")
+
+capitals <- read_csv("capitals.csv")
+
+head(capitals)
+tail(capitals)
+View(capitals)
+
+capitals <- read_csv("capitals_corrected.csv")
+
+head(capitals)
