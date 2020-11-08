9ff3d64bbac4975d4c2097e574a8af3b7408d0b4
sanazjamloo <sanaz.jamloo@gmail.com>
2020-11-07 23:30:38 -0800

    update unite

--- unite.R
+++ unite.R
@@ -17,3 +17,27 @@ regional_inspections <- unite(inspections, Region, City, State, sep=",")
 glimpse(regional_inspections)
 
 unique(regional_inspections$Region)
+
+library(stringr)
+
+#String manipulation
+# Converting everything in the region field to uppercase
+regional_inspections$Region  <- str_to_upper(regional_inspections$Region)
+# unique(regional_inspections$Region)
+
+# Handling misspellings of Chicago
+regional_inspections$Region <- str_replace(regional_inspections$Region, "CCHICAGO, IL", "CHICAGO, IL")
+regional_inspections$Region <- str_replace(regional_inspections$Region, "CHCICAGO, IL", "CHICAGO, IL")
+regional_inspections$Region <- str_replace(regional_inspections$Region, "CHICAGOCHICAGO, IL", "CHICAGO, IL")
+regional_inspections$Region <- str_replace(regional_inspections$Region, "CHCHICAGO, IL", "CHICAGO, IL")
+regional_inspections$Region <- str_replace(regional_inspections$Region, "CHICAGOI, IL", "CHICAGO, IL")
+
+unique(regional_inspections$Region)
+
+# Handling NAs
+regional_inspections$Region <- str_replace(regional_inspections$Region, "CHICAGO, NA", "CHICAGO, IL")
+unique(regional_inspections$Region)
+
+
+
+
